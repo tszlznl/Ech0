b6e4ddf66beb4bb2720b4e37e63e54a6f3b39d18
L1nSn0w <l1nsn0w@qq.com>
2026-03-04 23:03:51 +0800

    Update data icon in data.vue component to use Material Symbols design, enhancing visual consistency and modernizing the iconography. Adjust component name accordingly.

--- web/src/components/icons/data.vue
+++ web/src/components/icons/data.vue
@@ -1,10 +1,10 @@
 
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 14 14"><!-- Icon from Streamline by Streamline - https://creativecommons.org/licenses/by/4.0/ --><g fill="none" stroke="#888888" stroke-linecap="round" stroke-linejoin="round"><ellipse cx="7" cy="3" rx="6.5" ry="2.5" /><path d="M.5 3v8c0 1.38 2.91 2.5 6.5 2.5s6.5-1.12 6.5-2.5V3" /><path d="M13.5 7c0 1.38-2.91 2.5-6.5 2.5S.5 8.38.5 7" /></g></svg>
-  </template>
-  
-  <script lang="ts">
-  export default {
-    name: 'StreamlineComputerDatabaseRaidStorageCodeDiskProgrammingDatabaseArrayHardDisc'
-  }
-  </script>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="#888888" d="M12 21q-3.775 0-6.387-1.162T3 17V7q0-1.65 2.638-2.825T12 3t6.363 1.175T21 7v10q0 1.675-2.613 2.838T12 21m0-11.975q2.225 0 4.475-.638T19 7.025q-.275-.725-2.512-1.375T12 5q-2.275 0-4.462.638T5 7.025q.35.75 2.538 1.375T12 9.025M12 14q1.05 0 2.025-.1t1.863-.288t1.675-.462T19 12.525v-3q-.65.35-1.437.625t-1.675.463t-1.863.287T12 11t-2.05-.1t-1.888-.288T6.4 10.15T5 9.525v3q.625.35 1.4.625t1.663.463t1.887.287T12 14m0 5q1.15 0 2.338-.175t2.187-.462t1.675-.65t.8-.738v-2.45q-.65.35-1.437.625t-1.675.463t-1.863.287T12 16t-2.05-.1t-1.888-.288T6.4 15.15T5 14.525V17q.125.375.788.725t1.662.638t2.2.462T12 19" /></svg>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'MaterialSymbolsDatabaseOutline'
+}
+</script>
